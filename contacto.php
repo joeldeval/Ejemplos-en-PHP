fc72d4fc03dc32c37032d832eebfbf816052274b
Joel Valdivia <jooelvaldivi@gmail.com>
2014-01-06 11:03:29 -0600

    Create contacto.php

---
+++ contacto.php
@@ -0,0 +1,47 @@
+<?php
+require_once('conexion.php');
+
+class Contacto
+{
+    public $nombre;
+    public $ap;
+    public $am;
+    public $direccion;
+    public $telefono;
+    public $cp;
+
+    public function encontrarTodos()
+    {
+        try
+        {
+            $resultado = array();
+            $conexion = new Conexion();
+            if(!$conexion->conectar())
+            {
+                throw new Exception($conexion->getError());
+            }
+            $sql = "SELECT * FROM empleados;";
+            if ($result = $conexion->mysqli->query($sql))
+            {
+                if ($result->num_rows > 0) 
+                {
+                     while($row = $result->fetch_assoc())
+                     {
+                         $resultado[] = $row;
+                     }
+                }
+            }
+            $conexion->cerrar();
+            return $resultado;
+        }
+        catch(Exception $e)
+        {
+            return array();
+        } 
+    }
+
+
+  
+    }
+
+ ?>
