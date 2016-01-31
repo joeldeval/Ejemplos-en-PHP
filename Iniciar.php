1d2259280e82cee99aeabd945b6e0d53777996c0
Joel Valdivia <jooelvaldivi@gmail.com>
2016-01-31 00:04:49 -0600

    Update Iniciar.php

--- Iniciar.php
+++ Iniciar.php
@@ -1,15 +1,16 @@
 <?php
-   
+   // Obtiene datos de vista
     $Codigo = $_POST['txtUsuario'];
     $Password = $_POST['txtContra'];
+
 if( ($Codigo == 100 && $Password == "joel") ||
      ($Codigo == 110 && $Password == "tonala")||
       ($Codigo==1 && $Password=="root")){
-        session_start();
+        session_start(); // Inició de Sesión
         $_SESSION["codigo"] = $Password;
-        header("Location: empleados.php");
+        header("Location: empleados.php"); // Redirección a empleados
       }else{
-        header("Location: Sesion.php");
+        header("Location: Sesion.php"); // Redirección a acceder de nuevo
         }
 ?>
 
